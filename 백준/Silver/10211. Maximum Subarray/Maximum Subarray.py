2162fc31b5e9da17712acc9ec5029e4f1b23cff3
ae-chae <102152330+ae-chae@users.noreply.github.com>
2022-08-30 02:57:23 +0900

    [Silver IV] Title: Maximum Subarray, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    dp = [0] * N
+    dp[0] = arr[0]
+
+    for i in range(1, N):
+        dp[i] = max(dp[i-1] + arr[i], arr[i])
+    print(max(dp))
