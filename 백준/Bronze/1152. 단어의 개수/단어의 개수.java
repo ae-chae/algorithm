448d14525022af6b9c63110e0d4691b38e10c162
ae-chae <102152330+ae-chae@users.noreply.github.com>
2023-11-28 21:29:28 +0900

    [Bronze II] Title: 단어의 개수, Time: 536 ms, Memory: 30728 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringTokenizer str = new StringTokenizer(sc.nextLine(), " ");
+		System.out.println(str.countTokens());
+	}
+}
